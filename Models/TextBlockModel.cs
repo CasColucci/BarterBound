3eb203b604388b46fba08c36cc0c394af65ae22a
Cas <51390039+CasColucci@users.noreply.github.com>
2024-12-30 19:10:35 -0500

    Setting up some basic things

---
+++ Models/TextBlockModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarterBound.Models
+{
+    internal class TextBlockModel
+    {
+        public string TriggerEvent { get; set; }
+
+        public List<string> TextValues { get; set; }
+
+        public 
+    }
+}
